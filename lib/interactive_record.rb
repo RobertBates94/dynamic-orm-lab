8c3e85dace4faee99105b15e68d1d73bde5a108f
Robert <bates.to.infinity@gmail.com>
2021-07-27 19:51:38 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,60 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+    def self.table_name
+        "#{self.to_s.downcase}s"
+    end
+
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS #{self.table_name} (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            grade INTEGER
+        )
+        SQL
+        
+        DB[:conn].execute(sql)
+    end
+
+    def self.find(id)
+        sql = <<-SQL
+        SELECT * FROM #{self.table_name} WHERE id = ?
+        SQL
+
+        rows = DB[:conn].execute(sql, id)
+        self.reify_from_row(row.first)
+    end
+
+    def self.reify_from_row(row)
+        self.new.map do |s|
+            s.id = row[0]
+            s.name = row[1]
+            s.grade = row[2]
+        end
+    end
+
+    def insert
+        sql = <<-SQL
+            INSERT INTO #(self.table_name) (name, grade)
+            VALUES (?, ?)
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.grade)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid();").flatten.first
+    end
+
+    def update
+        sql = <<-SQL
+        UPDATE #{self.table_name} SET name = ?, grade = ?, WHERE id = ?
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.grade, self.id)
+    end
+
+    def save
+    end
+
   
 end
--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,20 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    attr_accessor :name, :grade
+    attr_reader :id
+
+    def initialize(id = nil, name, grade)
+        @id = id
+        @name = name
+        @grade = grade
+    end
+
+    def self.column_names(row)
+        s.id = row[0]
+        s.name = row[1]
+        s.grade = row[2]
+
+    end
+
 end
